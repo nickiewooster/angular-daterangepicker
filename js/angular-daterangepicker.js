c977053f7b7306881f4ba7b23f013401c450b878
Vu Quang <QVUKQ6@global.scd.scania.com>
2016-09-20 08:25:20 +0200

    updated to latest bootstrap-daterangepicker

--- js/angular-daterangepicker.js
+++ js/angular-daterangepicker.js
@@ -11,7 +11,7 @@
     }
   });
 
-  picker.directive('dateRangePicker', ['$compile', '$timeout', '$parse', 'dateRangePickerConfig', function($compile, $timeout, $parse, dateRangePickerConfig) {
+  picker.directive('dateRangePicker', function($compile, $timeout, $parse, dateRangePickerConfig) {
     return {
       require: 'ngModel',
       restrict: 'A',
@@ -204,6 +204,6 @@
         });
       }
     };
-  }]);
+  });
 
 }).call(this);
